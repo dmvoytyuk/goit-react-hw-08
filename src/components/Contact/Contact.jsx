0e693f8669ff17435df404f33d420236835d1352
dmvoytyuk <dmvoytyuk@gmail.com>
2024-04-30 10:52:17 +0200

    contact style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,43 +1,47 @@
 /* eslint-disable react/prop-types */
-import { useDispatch, useSelector } from "react-redux";
-import { selectToken } from "../../redux/auth/selectors.js";
-import { deleteContact } from "../../redux/contacts/operations.js";
+
+import { useDispatch, useSelector } from 'react-redux';
+import { selectToken } from '../../redux/auth/selectors.js';
+import { deleteContact } from '../../redux/contacts/operations.js';
 import {
-  selectDeletingContactId,
-  selectIsDeleting,
-} from "../../redux/contacts/selectors.js";
-import { setDeletingContactId } from "../../redux/contacts/slice.js";
-import Loader from "../Loader/Loader.jsx";
-import styles from "./Contact.module.css";
+	selectDeletingContactId,
+	selectIsDeleting,
+} from '../../redux/contacts/selectors.js';
+import { setDeletingContactId } from '../../redux/contacts/slice.js';
+import Loader from '../Loader/Loader.jsx';
+import styles from './Contact.module.css';
 
 const Contact = ({ contact }) => {
-  const dispatch = useDispatch();
-  const token = useSelector(selectToken);
-  const isDeleting = useSelector(selectIsDeleting);
-  const deletingContactId = useSelector(selectDeletingContactId);
+	const dispatch = useDispatch();
+	const token = useSelector(selectToken);
+	const isDeleting = useSelector(selectIsDeleting);
+	const deletingContactId = useSelector(selectDeletingContactId);
 
-  const onDelete = () => {
-    const data = {
-      token: token,
-      contactId: contact.id,
-    };
-    dispatch(setDeletingContactId(contact.id));
-    dispatch(deleteContact(data));
-  };
+	const onDelete = () => {
+		const data = {
+			token: token,
+			contactId: contact.id,
+		};
+		dispatch(setDeletingContactId(contact.id));
+		dispatch(deleteContact(data));
+	};
 
-  return (
-    <li className={styles.contact}>
-      <div>
-        <p>{contact.name}</p>
-        <p>{contact.number}</p>
-      </div>
-      {!isDeleting || deletingContactId !== contact.id ? (
-        <button onClick={onDelete}>Delete</button>
-      ) : (
-        deletingContactId === contact.id && <Loader color="#2F699E" />
-      )}
-    </li>
-  );
+	return (
+		<li className={styles.contact}>
+			<div>
+				<div>Contact Avatar</div>
+				<div>
+					<h4>{contact.name}</h4>
+					<p>{contact.number}</p>
+				</div>
+			</div>
+			{!isDeleting || deletingContactId !== contact.id ? (
+				<button onClick={onDelete}>Delete</button>
+			) : (
+				deletingContactId === contact.id && <Loader color="#2F699E" />
+			)}
+		</li>
+	);
 };
 
 export default Contact;
